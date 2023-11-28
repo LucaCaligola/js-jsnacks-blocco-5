82b355999a4e5d692ebbe60de9fe5b09c83b47a0
LucaCaligola <luca.caligola@gmail.com>
2023-11-28 12:40:54 +0100

    working on a function to get a random number

---
+++ snack-5/script.js
@@ -0,0 +1,20 @@
+// Creare un array di oggetti di squadre di volley.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti e falli subiti.
+// Generare numeri random al posto delle nelle proprietà:
+// punti fatti e falli subiti
+
+
+const array = 
+
+{
+    nome: 'Volley CLub', punti:'', falli:''
+
+}
+
+console.log(array.punti)
+
+function getRandomNumber(min,max) {
+    min = 1;
+    max = 90;
+    return Math.floor(math.random() * (max - min) + min);
+}
