5a2dd2365e2e3b398818e6a7599a2203d1b37a53
LucaCaligola <luca.caligola@gmail.com>
2023-11-28 12:32:13 +0100

    snack-4

---
+++ snack-4/script.js
@@ -0,0 +1,17 @@
+// Scrivi una funzione che fonda due array (aventi lo stesso numero di elementi) 
+// prendendo alternativamente gli elementi da uno e dall'altro
+// es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
+
+const numeri = ['1','2','3']
+const lettere = ['a','b','c']
+
+const sum = []
+
+function somma (numeri,lettere) {
+    for (i = 0; i = numeri.length; i++ ) {
+        
+        sum.push(numeri[i]),(lettere[i])
+    }
+}
+console.log(sum)
